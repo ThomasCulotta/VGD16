0f6f9b261d51e1f14342161d554d159231972d4b
struon2@gmail.com <struon2@gmail.com>
2016-02-16 18:02:56 -0600

    starting HP

--- SolarSmuggler/Assets/Scripts/PlayerController.cs
+++ SolarSmuggler/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    public float max_Health = 100f;
+    public float curr_Health = 0f;
+    public GameObject healthBar;
+
 	void Start()
 	{
 		// Force player to grid
 		transform.position = new Vector3(Mathf.Floor(transform.position.x), 0f, Mathf.Floor(transform.position.z));
 
-		// Initialize player stats...
+        // Initialize player stats...
+        curr_Health = max_Health; //set player to maximum health
+        InvokeRepeating("decreaseHealth", 1f, 1f); // just for testing purposes, this decreases health by 2 every second
 	}
 
 	void Update()
@@ -25,4 +31,19 @@ public class PlayerController : MonoBehaviour
 		iTween.MoveTo(gameObject, iTween.Hash(axis, position + ammount, "time", 0.5f));
 		transform.position = new Vector3(Mathf.Floor(transform.position.x), 0f, Mathf.Floor(transform.position.z));
 	}
+
+    void decreaseHealth()
+    {
+        curr_Health -= 2f; // whatever happens to player we decrease health
+
+        //need a ratio to from current health & max health to scale the hp bar
+        float calc_Health = curr_Health / max_Health;
+        setHealthBar(calc_Health);
+    }
+
+    public void setHealthBar(float healthVal)
+    {
+        //health value needs to be from 0 to 1
+        healthBar.transform.localScale = new Vector3(healthVal, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
 }
