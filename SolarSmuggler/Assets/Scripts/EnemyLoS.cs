46c08bb4cecfed8a5f13a6345523b8258a61acde
pblanc5 <pblanc5@lsu.edu>
2016-02-16 22:32:23 -0600

    Enemy Line of Sight

---
+++ SolarSmuggler/Assets/Scripts/EnemyLoS.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyLoS : MonoBehaviour {
+
+    private const float MAX_DIST = 50;
+    private Vector3 playerPos;
+    private GameObject player;
+    private RaycastHit hit;
+    
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("player");
+        playerPos = player.transform.position;
+	}
+	
+	
+	void Update () {
+	    if(Physics.Raycast(transform.position, playerPos, out hit, MAX_DIST))
+        {
+            Debug.Log("Raycast True\n");
+            if (hit.collider.tag.Equals("player"))
+            {
+                Debug.Log("spotted\n");
+            }
+        }
+	}
+}
