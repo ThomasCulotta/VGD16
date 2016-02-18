68ea8df63e6b6b3e19967fa6922c76985db15e43
Brian Chiang <bchian1@lsu.edu>
2016-02-18 10:52:52 -0600

    Cargo bar added with script. Can be edited later to fit with Derin's art.

--- SolarSmuggler/Assets/Scripts/PlayerController.cs
+++ SolarSmuggler/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
     public float max_Health = 100f;
     public float curr_Health = 100f;
+    public float max_Cargo = 100f; //If we ever decide for cargo size
+    public float curr_Cargo = 0f;
+    public Slider cargoBar;
     public GameObject healthBar;
+    public Text cargoText;
 
 	void Start()
 	{
@@ -14,7 +19,9 @@ public class PlayerController : MonoBehaviour
 
         // Initialize player stats...
         curr_Health = max_Health; //set player to maximum health
+        curr_Cargo = max_Cargo; //set cargo to maximum capacity
         //InvokeRepeating("decreaseHealth", 1f, 1f); just for testing purposes, this decreases health by 2 every second
+        SetCargoBar(curr_Cargo);
 	}
 
 	void Update()
@@ -24,7 +31,11 @@ public class PlayerController : MonoBehaviour
 		if (Input.GetKeyDown(KeyCode.A)) MovePlayer("x", transform.position.x, -1f);
 		if (Input.GetKeyDown(KeyCode.S)) MovePlayer("z", transform.position.z, -1f);
 		if (Input.GetKeyDown(KeyCode.D)) MovePlayer("x", transform.position.x,  1f);
-	}
+
+        //if (Input.GetKeyDown(KeyCode.M)) DecreaseCargo();For testing
+        //if (Input.GetKeyDown(KeyCode.I)) IncreaseCargo();For testing
+        SetCargoText();//Setting cargo amount text
+    }
 
 	void MovePlayer(string axis, float position, float ammount)
 	{
@@ -45,5 +56,35 @@ public class PlayerController : MonoBehaviour
     {
         //health value needs to be from 0 to 1
         healthBar.transform.localScale = new Vector3(healthVal, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
+
+    public void SetCargoBar(float cargo)
+    {
+        cargoBar.value = cargo;
+    }
+
+    public void DecreaseCargo()
+    {
+        if (curr_Cargo != 0)
+        {
+            curr_Cargo--;
+            float calc_Cargo = curr_Cargo / max_Cargo;
+            SetCargoBar(calc_Cargo);
+        }
+    }
+
+    public void IncreaseCargo()
+    {
+        if (curr_Cargo == max_Cargo)
+        {
+            curr_Cargo++;
+            float calc_Cargo = curr_Cargo / max_Cargo;
+            SetCargoBar(calc_Cargo);
+        }
+    }
+
+    void SetCargoText()
+    {
+        cargoText.text = "Cargo: " + curr_Cargo.ToString();
     }
 }
