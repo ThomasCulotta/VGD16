772a8de0ce8644dc4a8a5313fcc842c7b4478c55
struon2@gmail.com <struon2@gmail.com>
2016-02-29 19:12:07 -0600

    camera

---
+++ SolarSmuggler/Assets/Scripts/Test_Camera_Follow.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test_Camera_Follow : MonoBehaviour {
+
+    public Transform target;
+
+    [System.Serializable]
+    public class PositionSettings
+    {
+        //distance from our target
+        //bools for zooming and smoothfollowing
+        //min and max zoom settings
+        public float distanceFromTarget = -50;
+        public bool allowZoom           = true;
+        public float zoomSmooth         = 100;
+        public float zoomStep           = 2;
+        public float maxZoom            = -30;
+        public float minZoom            = -60;
+        public bool smoothFollow        = true;
+        public float smooth             = 0.05f;
+
+        [HideInInspector]
+        public float newDistance = -50; //used for smooth zooming & gives us a "destination" zoom
+    }
+
+    [System.Serializable]
+    public class OrbitSettings
+    {
+        //holindg current x and y rotation for camera
+        //bool for allowing orbit
+        public float xRotation      = -65;
+        public float yRotation      = -180;
+        public bool allowOribit     = true;
+        public float yOrbitSmooth   = 0.5f;
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public string MOUSE_ORBIT = "MouseOrbit";
+        public string ZOOM = "Mouse ScrollWheel";
+    }
+
+    public PositionSettings position    = new PositionSettings();
+    public OrbitSettings orbit          = new OrbitSettings();
+    public InputSettings input          = new InputSettings();
+
+    //target destination
+    Vector3 destination             = Vector3.zero;
+    Vector3 camVelocity             = Vector3.zero;
+    Vector3 currentMousePosition    = Vector3.zero;
+    Vector3 previousMousePosition   = Vector3.zero;
+    float mouseOrbitInput, zoomInput;
+
+    void Start()
+    {
+        //setting camera target
+        SetCameraTarget(target);
+
+        if (target)
+        {
+            moveToTarget();//moves camera to player
+        }
+    }
+
+    public void SetCameraTarget(Transform t)
+    {
+        //if we want to set a new target at runtime
+        target = t;
+        if (target == null)
+        {
+            Debug.LogError("Camera needs a target");
+        }
+    }
+
+    void getInput()
+    {
+        //filling values for our input variables
+        mouseOrbitInput = Input.GetAxisRaw(input.MOUSE_ORBIT);
+        zoomInput = Input.GetAxisRaw(input.ZOOM);
+    }
+
+    void Update()
+    {
+        //getting input and zooming
+        getInput();
+        if (position.allowZoom)
+        {
+            zoomInTarget();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //moveToTarget
+        //lookAtTarget
+        //orbit
+        if (target)
+        {
+            moveToTarget();
+            lookAtTarget();
+            mouseOrbitTarget();
+        }
+    }
+
+    void moveToTarget()
+    {
+        //handling getting our camera to its desintation position
+        destination = target.position;
+        destination += Quaternion.Euler(orbit.xRotation, orbit.yRotation, 0) * -Vector3.forward * position.distanceFromTarget; //quater * vec3 = vec3
+
+        //move camera to dest
+        if (position.smoothFollow)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, destination, ref camVelocity, position.smooth);
+        }
+        //otherwise just lock onto the target
+        else
+        {
+            transform.position = destination;
+        }
+    }
+
+    void lookAtTarget()
+    {
+        //handling getting our camera to look at the target at all times
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = targetRotation;
+    }
+
+    //orbiting target
+    void mouseOrbitTarget()
+    {
+        //getting the camera to orbit around our character
+        previousMousePosition = currentMousePosition;
+        currentMousePosition = Input.mousePosition;
+
+        //if mouse0 is pressed down
+        if(mouseOrbitInput > 0)
+        {
+            orbit.yRotation += (currentMousePosition.x - previousMousePosition.x) * orbit.yOrbitSmooth;
+        }
+    }
+
+    //zoom target
+    void zoomInTarget()
+    {
+        //modifying the distance from target to be closer/further away from target
+        position.newDistance += position.zoomStep * zoomInput;
+
+        position.distanceFromTarget = Mathf.Lerp(position.distanceFromTarget, position.newDistance, position.zoomSmooth * Time.deltaTime);
+
+        //maintain distance threshold
+        if(position.distanceFromTarget > position.maxZoom)
+        {
+            position.distanceFromTarget = position.maxZoom;
+            position.newDistance = position.maxZoom;
+        }
+        if (position.distanceFromTarget < position.minZoom)
+        {
+            position.distanceFromTarget = position.minZoom;
+            position.newDistance = position.minZoom;
+        }
+    }
+}
---
+++ SolarSmuggler/Assets/Scripts/Test_Movement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test_Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 4, 0f, Input.GetAxis("Vertical") * Time.deltaTime * 4);
+    }
+}
