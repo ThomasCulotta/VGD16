8709c968f9eb90d4fbb8102a79e9f401a14b8400
pblanc5 <pblanc5@lsu.edu>
2016-03-05 16:50:05 -0600

    Better documentation on EnemyLoS

--- SolarSmuggler/Assets/Scripts/EnemyLoS.cs
+++ SolarSmuggler/Assets/Scripts/EnemyLoS.cs
@@ -55,37 +55,33 @@ public class EnemyLoS : MonoBehaviour
 		 * pretentious douche. We might want to change it later to only raycast while the 
 		 * player or enemy is moving instead of continuously, but it probably won't matter too much.
 		 */
+
 		// Thomas: Added this small debug line so we'll see exactly what the ray is doing when we test this out.
 		Debug.DrawLine(transform.position, player.transform.position, Color.cyan, 0.5f);
 
-        /*
-         *Patrick:
-         *Fancy math that calculates the direction vector
-         */
-
+        //Fancy math that calculates the direction vector 
+        Vector3 heading = player.transform.position - transform.position;
+        if(Physics.Raycast(transform.position, heading, out hit, MAX_SPOT))
+        {
+            //Debug.Log("Hit True\n");
+            if (hit.collider.tag.Equals("Player"))
+            {
+                //Debug.Log("Spotted\n");
+                //Enemy reaction script goes here.
+                ShootAtPlayer();
+            }
+        }
       
-       Vector3 heading = player.transform.position - transform.position;
-       if(Physics.Raycast(transform.position, heading, out hit, MAX_SPOT))
-       {
-           Debug.Log("Hit True\n");
-           if (hit.collider.tag.Equals("Player"))
-           {
-               Debug.Log("Spotted\n");
-               //Enemy reaction script goes here.
-               ShootAtPlayer();
-           }
-       }
-      
-        
        StartCoroutine("initESP");
-       
 	}
+
     /*
      *Patrick: This will setup the Enemy Search Plane at the start 
      *of the Enemy's turn.
      */
     IEnumerator initESP()
     {
+        //the Enemy GridNode is primed
         EnemySearchPlane[MAX_MOVE, MAX_MOVE].coords    = new Vector3(transform.position.x, 0f, transform.position.z);
         EnemySearchPlane[MAX_MOVE, MAX_MOVE].visited   = true;
         EnemySearchPlane[MAX_MOVE, MAX_MOVE].hasPlayer = false;
@@ -93,6 +89,7 @@ public class EnemyLoS : MonoBehaviour
         EnemySearchPlane[MAX_MOVE, MAX_MOVE].x         = MAX_MOVE;
         EnemySearchPlane[MAX_MOVE, MAX_MOVE].y         = MAX_MOVE;
 
+        //Init the Queue to begin the Flood Fill
         BFSQueue = new Queue();
         BFSQueue.Enqueue(EnemySearchPlane[MAX_MOVE, MAX_MOVE]);
 
@@ -106,17 +103,16 @@ public class EnemyLoS : MonoBehaviour
         
         while (BFSQueue.Count != 0)
         {
-            //Get current node
+            //Get current node out of Queue
             GridNode cur = (GridNode)BFSQueue.Dequeue();
             int nextDist = cur.dist + 1;
 
-            //Player Node found
+            //Player Node found, exit function.
             if (EnemySearchPlane[cur.x, cur.y].hasPlayer)
             {
                 Debug.Log("Found Player at " + cur.x + "," + cur.y + " He is " + cur.dist + " away.");
                 return;
             }
-
 
             //Saving Computations
             int xP1 = cur.x + 1;
@@ -125,16 +121,6 @@ public class EnemyLoS : MonoBehaviour
             int yM1 = cur.y - 1;
 
             //Checking Boundaries and if visited
-           /* if (yP1 < MAX_SPOT && !EnemySearchPlane[cur.x, yP1].visited) BFSQueue.Enqueue(EnemySearchPlane[cur.x, yP1]); //North
-            if (yM1 > 0        && !EnemySearchPlane[cur.x, yM1].visited) BFSQueue.Enqueue(EnemySearchPlane[cur.x, yM1]); //South
-            if (xP1 < MAX_SPOT && !EnemySearchPlane[xP1, cur.y].visited) BFSQueue.Enqueue(EnemySearchPlane[xP1, cur.y]); //East
-            if (xM1 > 0        && !EnemySearchPlane[xM1, cur.y].visited) BFSQueue.Enqueue(EnemySearchPlane[xM1, cur.y]); //West
-
-            if (xP1 < MAX_SPOT && yP1 < MAX_SPOT && !EnemySearchPlane[xP1, yP1].visited) BFSQueue.Enqueue(EnemySearchPlane[xP1, yP1]); //NorthEast
-            if (xM1 > 0        && yM1 > 0        && !EnemySearchPlane[xM1, yM1].visited) BFSQueue.Enqueue(EnemySearchPlane[xM1, yM1]); //SouthWest
-            if (xP1 < MAX_SPOT && yM1 > 0        && !EnemySearchPlane[xP1, yM1].visited) BFSQueue.Enqueue(EnemySearchPlane[xP1, yM1]); //SouthEast
-            if (xM1 > 0        && yP1 < MAX_SPOT && !EnemySearchPlane[xM1, yP1].visited) BFSQueue.Enqueue(EnemySearchPlane[xM1, yP1]); //NorthWest */
-
             if (yP1 < MAX_SPOT && !EnemySearchPlane[cur.x, yP1].visited) visit(cur.x, yP1, nextDist); //North
             if (yM1 > 0        && !EnemySearchPlane[cur.x, yM1].visited) visit(cur.x, yM1, nextDist); //South
             if (xP1 < MAX_SPOT && !EnemySearchPlane[xP1, cur.y].visited) visit(xP1, cur.y, nextDist); //East
@@ -147,14 +133,12 @@ public class EnemyLoS : MonoBehaviour
 
             //Debug
             Debug.Log("["+cur.x+"," +cur.y+"]" + " Coords: " + cur.coords +  "; Visited: " + cur.visited + "; hasPlayer: " + cur.hasPlayer + "; Distance: " + cur.dist);
-        }
-        
+        } 
     }
 
     void visit(int x, int y, int dist)
     {
-
-        //Gamespace Coordinates
+        //Gamespace coordinates conversion
         float xCoord = x + transform.position.x - MAX_MOVE;
         float yCoord = y + transform.position.y - MAX_MOVE;
 
@@ -166,20 +150,19 @@ public class EnemyLoS : MonoBehaviour
         EnemySearchPlane[x, y].x         = x;
         EnemySearchPlane[x, y].y         = y;
         
-        //Player Found
+        //Player Found, info is loaded into a new Grid Node struct for easy access
         if (EnemySearchPlane[x, y].coords == player.transform.position)
         {
             EnemySearchPlane[x, y].hasPlayer = true;
             playerNode = EnemySearchPlane[x, y];
         }
 
+        //Enque so neighbors can be checked.
         BFSQueue.Enqueue(EnemySearchPlane[x, y]);
 
         //Visual Debugging
         GameObject debugCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
         debugCube.transform.position = new Vector3(xCoord, 0f, yCoord);
-        
-
     }
 
     void ShootAtPlayer()
