72bd7c634aee60455ac254ec5f3dbc5bc110ed3d
Brian Chiang <bchian1@lsu.edu>
2016-03-06 13:56:25 -0600

    Basic button functionality

---
+++ SolarSmuggler/Assets/Scripts/Buttons.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Buttons : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+    public Text text;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = Color.blue;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.color = Color.black; 
+    }
+}
---
+++ SolarSmuggler/Assets/Scripts/MenuController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject main;
+    public GameObject help;
+    public GameObject credit;
+
+	// Use this for initialization
+	void Start () {
+        main.SetActive(true);
+        help.SetActive(false);
+        credit.SetActive(false);
+	}
+
+    public void LoadHelp()
+    {
+        main.SetActive(false);
+        help.SetActive(true);
+    }
+
+    public void ExitHelp()
+    {
+        main.SetActive(true);
+        help.SetActive(false);
+    }
+
+    public void LoadCredit()
+    {
+        main.SetActive(false);
+        credit.SetActive(true);
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Thomas");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
