786e172f369ef20093fa166a71d859842e19c483
pblanc5 <pblanc5@lsu.edu>
2016-03-19 14:41:22 -0500

    Blue Noise spawn system finished

--- SolarSmuggler/Assets/Scripts/SpawnSystem.cs
+++ SolarSmuggler/Assets/Scripts/SpawnSystem.cs
@@ -7,6 +7,7 @@ public class SpawnSystem : MonoBehaviour {
     float MAX_GAME_AREA_X = 300;
     float MAX_GAME_AREA_Z = 300;
 
+    int MAX_CELLS = 5;
     int PICKUP_COUNT = 100;
 
     enum Pickups
@@ -24,36 +25,53 @@ public class SpawnSystem : MonoBehaviour {
 	
 	void spawnPickups()
     {
-        for (int i = 0; i < PICKUP_COUNT;  i++) {
-            float spawnX = Random.Range(0, 301);
-            float spawnZ = Random.Range(0, 301);
-            Pickups spawnType = (Pickups)Random.Range((int)Pickups.CUBE, (int)Pickups.COUNT);
+        float cellSize = MAX_GAME_AREA_X / MAX_CELLS;
 
-            switch (spawnType)
+        for (int i = 0; i < MAX_CELLS;  i++) {
+            float spawnMinX = cellSize * i;
+            float spawnMaxX = cellSize * (i+1);
+
+            spawnMinX += 5;
+            spawnMaxX -= 5;
+
+            for (int j = 0; j < MAX_CELLS; j++)
             {
-                case (Pickups.CUBE):
-                    {
-                        Debug.Log("Making a Cube");
-                        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                        Instantiate(cube, new Vector3(spawnX, 0f, spawnZ), Quaternion.identity);
-                    }
-                    break;
-                case (Pickups.SPHERE):
-                    {
-                        Debug.Log("Making a Sphere");
-                        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                        Instantiate(sphere, new Vector3(spawnX, 0f, spawnZ), Quaternion.identity);
-                    }
-                    break;
-                case (Pickups.CAPSULE):
-                    {
-                        Debug.Log("Making a Capsule");
-                        GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
-                        Instantiate(capsule, new Vector3(spawnX, 0f, spawnZ), Quaternion.identity);
-                    }
-                    break;
-            }
+                float spawnMinZ = cellSize * j;
+                float spawnMaxZ = cellSize * (j+ 1);
 
+                spawnMinZ += 5;
+                spawnMaxZ -= 5;
+
+                Pickups spawnType = (Pickups)Random.Range((int)Pickups.CUBE, (int)Pickups.COUNT);
+
+                int spawnPosX = (int)Random.Range(spawnMinX, spawnMaxX);
+                int spawnPosZ = (int)Random.Range(spawnMinZ, spawnMaxZ);
+
+                switch (spawnType)
+                {
+                    case (Pickups.CUBE):
+                        {
+                            Debug.Log("Making a Cube");
+                            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                            Instantiate(cube, new Vector3(spawnPosX, 0f, spawnPosZ), Quaternion.identity);
+                        }
+                        break;
+                    case (Pickups.SPHERE):
+                        {
+                            Debug.Log("Making a Sphere");
+                            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                            Instantiate(sphere, new Vector3(spawnPosX, 0f, spawnPosZ), Quaternion.identity);
+                        }
+                        break;
+                    case (Pickups.CAPSULE):
+                        {
+                            Debug.Log("Making a Capsule");
+                            GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                            Instantiate(capsule, new Vector3(spawnPosX, 0f, spawnPosZ), Quaternion.identity);
+                        }
+                        break;
+                }
+            }
         }
     }
 }
