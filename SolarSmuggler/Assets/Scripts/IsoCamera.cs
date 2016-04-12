479f3d6ca58817b8a2ba959f14bc8e0831660005
Thomas Culotta <thomasculotta94@gmail.com>
2016-04-12 11:51:19 -0500

    Camera movement/zooming refined

--- SolarSmuggler/Assets/Scripts/IsoCamera.cs
+++ SolarSmuggler/Assets/Scripts/IsoCamera.cs
@@ -4,14 +4,13 @@ using System.Collections;
 public class IsoCamera : MonoBehaviour
 {
     private const float CAM_SPEED = 6f;
-    private const float ROT_SPEED = 50f;
-    private const float ZOOM_SPEED = 10f;
+    private const float ROT_SPEED = 35f;
+    private const float ZOOM_SPEED = 50f;
 
     private GameObject player;
     private GameObject cam;
 
-    private float deltaX;
-    private float deltaZ;
+    private Vector3 deltaCam;
     private float deltaRotY;
     private float deltaZoom;
     private bool  moveToPlayer;
@@ -24,65 +23,56 @@ public class IsoCamera : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player");
         cam = transform.GetChild(0).gameObject;
-        moveToPlayer = false;
+        cam.transform.LookAt(transform.position);
+        moveToPlayer = true;
 	}
 	
 	void FixedUpdate()
     {
-        deltaX = 0f;
-        deltaZ = 0f;
+        deltaCam = Vector3.zero;
         deltaRotY = 0f;
         prevMousePos = curMousePos;
         curMousePos = Input.mousePosition;
 
         if (Input.GetKeyDown(KeyCode.Tab))
-        {
             moveToPlayer = !moveToPlayer;
-        }
 
         // Translation X and Z
         if (moveToPlayer)
         {
-            if (Mathf.Abs(transform.position.x - player.transform.position.x) > 0.25f)
-                deltaX = (player.transform.position.x - transform.position.x) / 5f;
-            if (Mathf.Abs(transform.position.z - player.transform.position.z) > 0.25f)
-                deltaZ = (player.transform.position.z - transform.position.z) / 5f;
+            deltaCam = player.transform.position - transform.position;
+            if (deltaCam.magnitude < 0.05f)
+                deltaCam = Vector3.zero;
         }
         else
-        {
-            deltaX = Input.GetAxisRaw("Horizontal");
-            deltaZ = Input.GetAxisRaw("Vertical");
-        }
+            deltaCam = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         
         // Rotation Y
         if (Input.GetMouseButton(0))
-        {
-            //if mouse0 is pressed down
-            deltaRotY = (curMousePos.x - prevMousePos.x);
-        }
+            deltaRotY = prevMousePos.x - curMousePos.x;
 
-        deltaZoom = Input.GetAxis("Mouse ScrollWheel");
-        if (cam.transform.localPosition.z > -3f && deltaZoom > 0)
-            deltaZoom = 0f;
-        else if (cam.transform.localPosition.z < -10f && deltaZoom < 0)
+        // Zoom Z
+        deltaZoom = Mathf.Clamp(Input.GetAxis("Mouse ScrollWheel"), -1f, 1f);
+        if ((cam.transform.localPosition.z > -3f && deltaZoom > 0) ||
+            (cam.transform.localPosition.z < -10f && deltaZoom < 0))
             deltaZoom = 0f;
     }
 
 	void Update()
     {
-        if (deltaX != 0 || deltaZ != 0)
+        if (deltaCam.magnitude != 0)
         {
             if (moveToPlayer)
-                transform.position = transform.position + new Vector3(deltaX * CAM_SPEED * Time.deltaTime, 0f, deltaZ * CAM_SPEED * Time.deltaTime);
+                transform.position = transform.position + deltaCam * CAM_SPEED * Time.deltaTime / 3f;
             else
-                transform.Translate(new Vector3(deltaX * CAM_SPEED * Time.deltaTime, 0f, deltaZ * CAM_SPEED * Time.deltaTime));
+                transform.Translate(deltaCam * CAM_SPEED * Time.deltaTime);
         }
         
         if (deltaRotY != 0)
             transform.Rotate(0f, deltaRotY * ROT_SPEED * Time.deltaTime, 0f);
 
         if (deltaZoom != 0)
-            cam.transform.Translate(Vector3.forward * deltaZoom * ZOOM_SPEED);
+            cam.transform.Translate(Vector3.forward * deltaZoom * ZOOM_SPEED * Time.deltaTime);
     }
 
     public void DamageIndicator()
