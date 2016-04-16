a83ad4d5a41925f2bcb8fa5440a58c4f8f9e9417
pblanc5 <pblanc5@lsu.edu>
2016-04-15 20:40:13 -0500

    Moon and Space Station changes

--- SolarSmuggler/Assets/Scripts/SpawnSystem.cs
+++ SolarSmuggler/Assets/Scripts/SpawnSystem.cs
@@ -42,7 +42,14 @@ public class SpawnSystem : MonoBehaviour {
     private Transform frostChild;
     private Transform purpleChild;
     private Transform waterChild;
-    private Transform moonChild;
+
+    private Transform frostMoon;
+    private Transform purpleMoon;
+    private Transform waterMoon;
+
+    private Transform frostMoonChild;
+    private Transform purpleMoonChild;
+    private Transform waterMoonChild;
 
     private GameObject cargo;
     private GameObject player;
@@ -51,6 +58,7 @@ public class SpawnSystem : MonoBehaviour {
     private GameObject sun;
     private GameObject UI;
     private GameObject spaceStation;
+    private Transform spaceStationChild;
     private GameObject AudioController;
     
 
@@ -84,23 +92,34 @@ public class SpawnSystem : MonoBehaviour {
         frost  = (GameObject)Resources.Load("Prefabs/Ice Planet", typeof(GameObject));
         purple = (GameObject)Resources.Load("Prefabs/Purple Planet", typeof(GameObject));
         water  = (GameObject)Resources.Load("Prefabs/Planet 2", typeof(GameObject));
-        moon   = (GameObject)Resources.Load("Prefabs/Moon", typeof(GameObject));
-
-        //Planet Childs
+        
+        //Planet Children
         frostChild  = frost.transform.FindChild("Planet 0");
         purpleChild = purple.transform.FindChild("Planet 0");
         waterChild  = water.transform.FindChild("Planet 0");
-        moonChild   = moon.transform.FindChild("Moon 0");
+
+        //Moons
+        frostMoon  = frostChild.transform.FindChild("Moon");
+        purpleMoon = purpleChild.transform.FindChild("Moon");
+        waterMoon  = waterChild.transform.FindChild("Moon");
+
+        //Moon Children
+        frostMoonChild  = frostMoon.transform.FindChild("Moon 0");
+        purpleMoonChild = purpleMoon.transform.FindChild("Moon 0");
+        waterMoonChild  = waterMoon.transform.FindChild("Moon 0");
 
         //Other Prefabs
-        cargo           = (GameObject)Resources.Load("Prefabs/Supplies");
-        player          = (GameObject)Resources.Load("Prefabs/Player");
-        enemy           = (GameObject)Resources.Load("Prefabs/Enemy");
-        cameraMaster    = (GameObject)Resources.Load("Prefabs/Game Camera");
-        UI              = (GameObject)Resources.Load("Prefabs/Player UI");
-        sun             = (GameObject)Resources.Load("Prefabs/Sun");
-        spaceStation    = (GameObject)Resources.Load("Prefabs/Space Station");
-        AudioController = (GameObject)Resources.Load("Prefabs/AudioController");
+        cargo             = (GameObject)Resources.Load("Prefabs/Supplies");
+        player            = (GameObject)Resources.Load("Prefabs/Player");
+        enemy             = (GameObject)Resources.Load("Prefabs/Enemy");
+        cameraMaster      = (GameObject)Resources.Load("Prefabs/Game Camera");
+        UI                = (GameObject)Resources.Load("Prefabs/Player UI");
+        sun               = (GameObject)Resources.Load("Prefabs/Sun");
+        spaceStation      = (GameObject)Resources.Load("Prefabs/Space Station Null");
+        AudioController   = (GameObject)Resources.Load("Prefabs/AudioController");
+
+        //Other Prefab Children
+        spaceStationChild = spaceStation.transform.FindChild("Space Station");
 
         /***********************************************************************************/
 
@@ -134,7 +153,7 @@ public class SpawnSystem : MonoBehaviour {
         MAX_ENEMIES   = SpawnMaster.SMALL_ENEMY;
 
         center = new Vector3(MAX_GAME_AREA / 2, 0, MAX_GAME_AREA / 2);
-        posAvailable = new bool[(MAX_GAME_AREA * MAX_GAME_AREA) + MAX_GAME_AREA];
+        posAvailable = new bool[(MAX_GAME_AREA * MAX_GAME_AREA) + MAX_GAME_AREA + 1];
         initAvaiableSpots();
         transform.position = center;
 
@@ -156,7 +175,7 @@ public class SpawnSystem : MonoBehaviour {
         MAX_ENEMIES   = SpawnMaster.MEDIUM_ENEMY;
 
         center = new Vector3(MAX_GAME_AREA / 2, 0, MAX_GAME_AREA / 2);
-        posAvailable = new bool[(MAX_GAME_AREA * MAX_GAME_AREA) + 1];
+        posAvailable = new bool[(MAX_GAME_AREA * MAX_GAME_AREA) + MAX_GAME_AREA + 1];
         initAvaiableSpots();
         transform.position = center;
 
@@ -178,7 +197,7 @@ public class SpawnSystem : MonoBehaviour {
         MAX_ENEMIES   = SpawnMaster.LARGE_ENEMY;
 
         center = new Vector3(MAX_GAME_AREA / 2, 0, MAX_GAME_AREA / 2);
-        posAvailable = new bool[(MAX_GAME_AREA * MAX_GAME_AREA) + 1];
+        posAvailable = new bool[(MAX_GAME_AREA * MAX_GAME_AREA) + MAX_GAME_AREA + 1];
         initAvaiableSpots();
         transform.position = center;
 
@@ -212,19 +231,26 @@ public class SpawnSystem : MonoBehaviour {
             GameObject planet = makePlanet(spawnPos);
             GameObject myPlanet = (GameObject)Instantiate(planet, center, Quaternion.identity);
             int hasMoon = Random.Range(0, 2);
-            if(hasMoon == 1)
+            if(hasMoon == 0)
             {
-                spawnMoon(planet.transform.GetChild(0).transform, spawnPos);
+                //spawnMoon(planet.transform.GetChild(0).transform, spawnPos);
+                var children = new System.Collections.Generic.List<GameObject>();
+                foreach(Transform child in myPlanet.transform.FindChild("Planet 0"))
+                    children.Add(child.gameObject);
+                children.ForEach(child => Destroy(child));
             }
         }
 
     }
 
+    /*
     void spawnMoon(Transform parent, Vector3 spawnPos)
     {
         GameObject myMoon = (GameObject)Instantiate(moon, spawnPos, Quaternion.identity);
         myMoon.transform.parent = parent;
     }
+    */
+
 
     void spawnCargoWhite(int numCargo)
     {
@@ -284,7 +310,8 @@ public class SpawnSystem : MonoBehaviour {
 
         AddToList(spawnPos);
         spaceStation.transform.localScale = new Vector3(1, 1, 1);
-        Instantiate(spaceStation, spawnPos, Quaternion.identity);
+        spaceStationChild.position = spawnPos - center;
+        Instantiate(spaceStation, center, Quaternion.identity);
     }
 
     void spawnPlayer()
@@ -307,6 +334,7 @@ public class SpawnSystem : MonoBehaviour {
         initCamera(spawnPos);
     }
 
+    /*
     void createTexture(GameObject moon)
     {
         
@@ -333,6 +361,7 @@ public class SpawnSystem : MonoBehaviour {
         moonTerrain.Apply();
         moon.GetComponent<Renderer>().material.mainTexture = moonTerrain;
     }
+    */
 
     void initAvaiableSpots()
     {
@@ -399,29 +428,37 @@ public class SpawnSystem : MonoBehaviour {
     {
         Planets spawnType = (Planets)Random.Range((int)Planets.FROST, (int)Planets.COUNT);
         int size = Random.Range(3, 8);
+        float moonSize = Random.Range(0.1f, 0.5f);
+        Vector3 spawnPoint = spawnPos - center;
 
         switch (spawnType)
         {
             case (Planets.FROST):
                 {
                     Debug.Log("Making FROST");
-                    frostChild.position = spawnPos - center;
-                    frostChild.localScale= new Vector3(size, size, size);
+                    frostChild.position = spawnPoint;
+                    frostChild.localScale = new Vector3(size, size, size);
+                    frostMoon.position = spawnPoint;
+                    frostMoonChild.localScale = new Vector3(moonSize, moonSize, moonSize);
                     return frost;
 
                 }
             case (Planets.PURPLE):
                 {
                     Debug.Log("Making Purple");
-                    purpleChild.position = spawnPos - center;
+                    purpleChild.position = spawnPoint;
                     purpleChild.localScale = new Vector3(size, size, size);
+                    purpleMoon.position = spawnPoint;
+                    purpleMoonChild.localScale = new Vector3(moonSize, moonSize, moonSize);
                     return purple;
                 }
             case (Planets.WATER):
                 {
                     Debug.Log("WATER");
-                    waterChild.position = spawnPos - center;
+                    waterChild.position = spawnPoint;
                     waterChild.localScale = new Vector3(size, size, size);
+                    waterMoon.position = spawnPoint;
+                    waterMoonChild.localScale = new Vector3(moonSize, moonSize, moonSize);
                     return water;
                 }
         }
