01c5eb9b4b439e7d8897b872a4805bbb39c9ccbf
struon2@gmail.com <struon2@gmail.com>
2016-04-26 20:05:40 -0500

    Score script

---
+++ SolarSmuggler/Assets/Scripts/StatsScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsScript : MonoBehaviour {
+
+    public GUIText health;
+    public GUIText cargo;
+    public GUIText turns;
+
+	// Use this for initialization
+	void Start () {
+        GameObject player = GameObject.FindWithTag("Player");
+        PlayerController playerController = player.GetComponent<PlayerController>();
+        health.text = playerController.curr_Health.ToString();
+        cargo.text  = playerController.curr_Cargo.ToString();
+        turns.text = playerController.turnCount.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
